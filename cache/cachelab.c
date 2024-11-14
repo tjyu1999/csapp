83d7c29f43545c834a2e02a48342cdd7c3103442
tjyu1999 <tjyu1999@gmail.com>
2024-11-14 22:22:57 +0800

    edit cache/cachelab.c

--- cache/cachelab.c
+++ cache/cachelab.c
@@ -1,6 +1,7 @@
 /*
  * cachelab.c - Cache Lab helper functions
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -16,7 +17,7 @@ int func_counter = 0;
  */
 void printSummary(int hits, int misses, int evictions) {
     printf("hits:%d misses:%d evictions:%d\n", hits, misses, evictions);
-    FILE* output_fp = fopen(".csim_results", "w");
+    FILE *output_fp = fopen(".csim_results", "w");
     assert(output_fp);
     fprintf(output_fp, "%d %d %d\n", hits, misses, evictions);
     fclose(output_fp);
