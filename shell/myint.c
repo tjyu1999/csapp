c8e82edc77968b71ebe15b2c914cd6ea8b9e858c
tjyu1999 <tjyu1999@gmail.com>
2024-12-26 23:30:58 +0800

    edit shell/myint.c

--- shell/myint.c
+++ shell/myint.c
@@ -5,6 +5,7 @@
  * Sleeps for <n> seconds and sends SIGINT to itself.
  *
  */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -12,25 +13,22 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-int main(int argc, char **argv) 
-{
-    int i, secs;
+int main(int argc, char **argv) {
+    int secs;
     pid_t pid; 
 
     if (argc != 2) {
-	fprintf(stderr, "Usage: %s <n>\n", argv[0]);
-	exit(0);
+	    fprintf(stderr, "Usage: %s <n>\n", argv[0]);
+	    exit(0);
     }
+	
     secs = atoi(argv[1]);
-
-    for (i=0; i < secs; i++)
-       sleep(1);
+    for (int i = 0; i < secs; ++i)
+        sleep(1);
 	
     pid = getpid(); 
-
     if (kill(pid, SIGINT) < 0)
-       fprintf(stderr, "kill (int) error");
+        fprintf(stderr, "kill (int) error");
 
     exit(0);
-
 }
